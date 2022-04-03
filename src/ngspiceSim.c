733397599950f976d370ef87f5a29c8836067a74
David Apple <appledav@ksu.edu>
2022-04-03 17:22:38 -0500

    Without testing server capabilities, ngspice sims correctly

--- src/ngspiceSim.c
+++ src/ngspiceSim.c
@@ -7,7 +7,7 @@
 /* we only care about the string in this case, the id and retPtr are nothing useful*/
 static void printFunction(char *string, int id, void *retPtr)
 {
-    printf("NGSPICE STRING: %s\n", string);
+    printf("%s\n", string);
     /* do some file stuff here */
 }
 
@@ -16,9 +16,10 @@ SendChar* func;
 int initNgspice()
 {
     /* bind printFunction to the function pointer ngspice_init expects */
-    func = &printFunction;
+    func = (SendChar*)&printFunction;
     ngSpice_Init(func, NULL, NULL, NULL, NULL, NULL, NULL);
 
+    printf("[DEBUG]: Init OK\n");
     return 0;
 }
 
@@ -28,16 +29,18 @@ int simNetlistFromSocket(char *netlistFileName, char *outputChoice)
         return -1;
     
     /* sourcing the file (loading it) */
-    char* sourceCommand = ""; 
+    char* sourceCommand = malloc(sizeof(char) * 256); 
     snprintf(sourceCommand, 256, "source %s", netlistFileName);
+    printf("[DEBUG]: Source Command: %s\n", sourceCommand);
     ngSpice_Command(sourceCommand);
     
     /* run the simulation */
     ngSpice_Command("op");
 
     /* building print string with user input*/
-    char* printCommand = "";
+    char* printCommand = malloc(sizeof(char) * 256);
     snprintf(printCommand, 256, "print %s", outputChoice);
+    printf("[DEBUG]: Print Command: %s\n", printCommand);
     ngSpice_Command(printCommand);
     
     return 0;
--- src/sspice.c
+++ src/sspice.c
@@ -5,19 +5,18 @@
 #include "../inc/serverListener.h"
 #include "../inc/ngspiceSim.h"
 
-#include </usr/local/include/ngspice/sharedspice.h>
 
 #define PORT 8080
 
 int main(int argc, char** argv)
 {
     /* begin with setting up the socket and returning the filename to be passed to ngspice */
-    char* fileName = initServerListener(PORT);
+    //char* fileName = initServerListener(PORT);
 
     /* init the simulation engine */
     initNgspice();
 
     /* after init, we are free to simulate */
     // TODO: find a way to get back the output choice 
-    simNetlistFromSocket(fileName, "node 2");
+    simNetlistFromSocket("../netlist.net", "node2");
 }
